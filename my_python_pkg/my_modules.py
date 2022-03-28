3fb7941931c654e5eb415ed46c54a53573808465
Keigo-Yonezawa <keigo20000306@gmail.com>
2022-03-29 02:30:05 +0900

    add my_modules/boxplot_group_by_sns()

---
+++ my_python_pkg/my_modules.py
@@ -0,0 +1,15 @@
+def boxplot_group_by_sns(X, group_label, target_label):
+    index_sorted = X.groupby(group_label)[target_label].mean().sort_values().index
+    labels = []
+    Y = []
+    
+    for i in index_sorted:
+        labels.append(i)
+        Y.append(X.groupby(group_label).get_group(i)[target_label])
+        
+    fig,ax = plt.subplots()
+    ax.boxplot(Y,labels=labels,vert=False)
+    plt.xlabel(target_label)
+    plt.ylabel(group_label)
+    plt.grid(True)
+    plt.show()
